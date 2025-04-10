10fd12fa9007ee161db0c7d0ab6238bab07fed59
Irina <irina.redactor@gmail.com>
2025-04-10 16:26:30 +0100

    upgraded structure to store author, track name and times listened to

--- lib/music_class.py
+++ lib/music_class.py
@@ -1,9 +1,39 @@
-class MusicClass():
-    def __init__(self):
-        self.storage = []
+"""As a user
+So that I can keep track of my music listening
+I want to add tracks I've listened to and see a list of them.
+"""
 
-    def add(self, track):
-        self.storage.append(track)
+class MusicClass():
+
+    def __init__(self):
+        self.storage =[]
+
+    def check_track_exists(self, track):
+        for index, dictionary in enumerate(self.storage):
+            if dictionary['track_name'] == track:
+                return index
+                
+        
+
+    def add(self, author, track):
+        index = self.check_track_exists(track)
+        if index == None:
+            track_info = {'author' : author,
+                      'track_name' : track,
+                      'times_played' : 1}
+            self.storage.append(track_info)
+            
+        else:
+            self.storage[index]["times_played"] += 1
+
 
     def list_tracks(self):
         return self.storage
+    
+
+
+#dict structure:
+#{author: name
+# track: track_name
+# times_played: 1}
+
--- test/test_music_class.py
+++ test/test_music_class.py
@@ -8,21 +8,33 @@ def test_class_has_a_storage():
 
 def test_adds_new_item_to_the_list():
     tunes = MusicClass()
-    tunes.add("Du Hast")
+    tunes.add("Rammstein", "Du Hast")
+    tunes.add("Rammstein", "Du Hast")
+    tunes.add("Chicago_musical", "All that jazz")
 
-    assert tunes.storage == ["Du Hast"]
+    assert tunes.storage == [{"author": "Rammstein",
+                              "track_name": "Du Hast",
+                              "times_played": 2},
+                              {"author": "Chicago_musical",
+                              "track_name": "All that jazz",
+                              "times_played": 1}]
 
 def test_returns_list():
     tunes = MusicClass()
-    tunes.add("Du Hast")
+    tunes.add("Rammstein", "Du Hast")
     result = tunes.list_tracks()
 
     assert isinstance(result, list)
 
 def test_list_returns_list_of_tracks_added():
     tunes = MusicClass()
-    tunes.add("Du Hast")
-    tunes.add("All that jazz")
+    tunes.add("Rammstein", "Du Hast")
+    tunes.add("Chicago_musical", "All that jazz")
     result = tunes.list_tracks()
 
-    assert result == ["Du Hast", "All that jazz"]
+    assert result == [{"author": "Rammstein",
+                              "track_name": "Du Hast",
+                              "times_played": 1},
+                              {"author": "Chicago_musical",
+                              "track_name": "All that jazz",
+                              "times_played": 1}]
